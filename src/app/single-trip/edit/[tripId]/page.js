0f737d4c98f057e74c17f9ba6a28bd00f75e50c5
Britnay Gore <britnaygore268@gmail.com>
2025-02-02 20:19:27 -0500

    Added template for component in the page routes

---
+++ src/app/single-trip/edit/[tripId]/page.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function EditTrip() {
+  return <div>Edit Trip page</div>;
+}
---
+++ src/app/trip-activities/[tripId]/edit/[activityId]/page.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function EditTripActivity() {
+  return <div>Activity Form for editting</div>;
+}
